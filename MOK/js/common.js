adf8b316b039204d08266be5016c45d0e2f22459
team-mok <51256212+team-mok@users.noreply.github.com>
2019-06-13 21:56:00 +0900

    Add files via upload

---
+++ MOK/js/common.js
@@ -0,0 +1,59 @@
+$(function(){
+var isSP;
+    $(window).on('load resize', function(){
+        if ( window.innerWidth > 767 ) {
+            isSP = false;
+        } else if ( window.innerWidth < 768 ) {
+            isSP = true;
+        }
+    });
+
+    var classVal = $('article').attr('class');
+    var classVals = classVal.split(' ');
+    $('.g-nv ul li').each(function(){
+        var dirName = $(this).find('a').attr('href');
+        for (var i = 0; i < classVals.length; i++) {
+            if (classVals[i] == 'index') {
+                classVals[i] = 'home';
+            }
+            if (dirName == '/') {
+                dirName = 'home';
+            }
+            if (dirName.match(classVals[i])) {
+                $(this).addClass('active');
+            }
+        }
+    });
+
+//アコーディオン、ハンバーガー
+    $('.menu').on('click', function(){
+        $('header .menu').toggleClass('pushed');
+        $('header').toggleClass('opened');
+    });
+
+
+  //スクロール→ヘッダーフェード
+    var startPos = 0,winScrollTop = 0;
+    var t = $('.main-visual').offset().top; 
+    var p = t + $('.main-visual').height();
+    $(window).on('scroll',function(){
+        winScrollTop = $(this).scrollTop();
+        if (winScrollTop >= startPos) {
+            if(winScrollTop >= 200){
+                $('header').fadeOut(700);;
+            }
+        } else {
+            $('header').fadeIn(700);;
+        }
+    //スクロールメインビジュアルクラス
+        if($(window).scrollTop() > p){
+        	$('header').addClass('white'); 
+      	}else{
+        	$('header').removeClass('white'); 
+      }
+
+        startPos = winScrollTop;
+    });
+
+});
+
